5ac6d669ebf5c5424573483f4c271d8288353327
Asad <asad.zahid840@gmail.com>
2020-11-27 10:01:47 +0500

    3rd Task

--- App.js
+++ App.js
@@ -1,25 +1,54 @@
 import React, { useState } from 'react';
-import { Text, View, StyleSheet, TextInput, Button,ScrollView ,TouchableOpacity} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  ScrollView,
+  TouchableOpacity,
+   Keyboard,
+} from 'react-native';
 import Constants from 'expo-constants';
 
-
 export default function App() {
+  const [getdprice, setdprice] = useState('');
+  const [getoprice, setoprice] = useState('');
+  const [getdper, setdper] = useState('');
+  const [getSList, setSList] = useState([
+    { key: '1', data: 'asad' },
+    { key: '2', data: 'ahmed' },
+    { key: '3', data: 'zahid' },
+  ]);
+  const calculatedprice = () => {
+    var x = (getdper * getoprice) / 100
+    x = getoprice - x
+    console.log(x)
+    setdprice(x)
+  };
   return (
     <View style={styles.container}>
-        <View>
-           <Text style={styles.paragraph}>
-             Shopping Discount Calculator
-           </Text>
-        </View>
-        <View style={styles.InputContainer}>
-           <TextInput style={styles.textinput}
-                 placeholder="Orignal Price"
-           ></TextInput>
-           <TextInput style={styles.textinput}
-                 placeholder="Discount Percentage"
-           >
-           </TextInput>
-        </View>
+      <View>
+        <Text style={styles.paragraph}>Shopping Discount Calculator</Text>
+      </View>
+      <View style={styles.InputContainer}>
+        <TextInput
+          style={styles.textinput}
+          placeholder="Orignal Price"
+          onChangeText={(text) => {setoprice(text);}}
+          keyboardType={'number-pad'}
+          value={getoprice}
+        />
+        <TextInput style={styles.textinput} placeholder="Discount Percentage" onChangeText={(text) => {
+            setdper(text);
+          }}
+          value={getdper} />
+          <Button title="Calculate" style={styles.cbutton} onPress={() => calculatedprice()}></Button>
+      </View>
+      <View>
+        <Text>After Discount: {getdprice}</Text>
+        <Text>You saved: {getoprice-getdprice}</Text>
+      </View>
     </View>
   );
 }
@@ -44,5 +73,23 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  scrollviewtext: {
+    fontSize: 18,
+    color: 'white',
+  },
+  cbutton:{
+
+  },
+  ScrollViewItem: {
+    width: '100%',
+    backgroundColor: 'orange',
+    alignSelf: 'center',
+    padding: 10,
+    margin: 5,
+    borderRadius: 10,
+    flexDirection: 'row',
+    alignItems: 'Center',
+    justifyContent: 'Space-Between',
   },
 });
